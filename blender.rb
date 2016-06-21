abd79fc4a30012ecacfab3a10712d707ed8b4e99
kevin <kevinochieng548@gmail.com>
2016-06-21 20:42:00 +0300

    code magnet

---
+++ blender.rb
@@ -0,0 +1,16 @@
+class Blender
+
+  def close_lid
+    puts "Sealed tight!"
+  end
+
+  def blend(speed)
+    puts "Spinning on #{speed} setting"
+  end
+
+  blender = Blender.new
+  
+  blender.close_lid
+  blender.blend("high")
+
+end
