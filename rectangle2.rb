53efc8040e41dc3b4c55aca181c79b0e8b0390c0
kevin <kevinochieng548@gmail.com>
2016-06-22 13:06:38 +0300

    raise error incase of negative values

--- rectangle2.rb
+++ rectangle2.rb
@@ -1,11 +1,14 @@
 class Rectangle
   attr_accessor :length,:width
   def area
+    if length < 0 || width < 0
+      raise "Value cannot be negative!!!"
+    end
     length*width
   end
 end
 
 rect = Rectangle.new
-rect.width = 5
+rect.width = 6
 rect.length= 4
 puts "The area for Rectangle with width #{rect.width} and length #{rect.length} is #{rect.area}"
