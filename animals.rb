220e489a48f9d240eddd233b714891011145f569
kevin <kevinochieng548@gmail.com>
2016-06-23 16:08:22 +0300

    override method move in Amadilo class

--- animals.rb
+++ animals.rb
@@ -1,43 +1,70 @@
-class Bird
+class Animal
+
+  #write a new value to @name
+ attr_accessor :name, :age
+
+ def name=(value)
+   if value == ""
+     raise "Name can't be blank"
+   end
+   @name = value
+ end
+ def age=value
+   if value<0
+     raise "An age of #{value} is not valid"
+   end
+   @age=value
+ end
+ def report_age
+    puts "#{@name} is #{@age} years old."
+  end
   def talk
-    puts "Chirp! Chirp!"
+    puts "#{@name} says Bark!"
   end
   def move(destination)
-    puts "Flying to the #{destination}."
+    puts "#{@name} runs to the #{destination}."
   end
 end
 
-class Dog
-  def talk
-    puts "Bark"
+class Dog < Animal
+  def to_s
+    "#{@name} the dog,age #{age}" 
   end
-  def move(destination)
-    puts "Running to the #{destination}."
+
+end
+
+class Bird < Animal
+ def talk
+    puts "#{@name} says Chirp! Chirp!"
   end
 end
 
-class Cat
-  def talk
-    puts "Meow!"
-  end
-  def move(destination)
-    puts "Running to the #{destination}."
+class Cat < Animal
+ def talk
+    puts "#{@name} says Meow!"
   end
 end
 
-#create new instances of the classes
-bird = Bird.new
-dog  = Dog.new
-cat  = Cat.new
+class Amadillo < Animal
+  def move(destination)
+    puts "#{@name} unrolls"
+    super
+  end
+end
 
-#call methods on the instances
-bird.move("tree")
-dog.talk
-bird.talk
-cat.move("house")
+whiskers = Cat.new
+whiskers.name = "Whiskers"
 
+fido = Dog.new
+fido.name = "Fido"
 
+polly = Bird.new
+polly.name = "Polly"
+polly.age = 2
+polly.talk
 
-
+polly.report_age
+fido.move("yard")
+whiskers.talk
 
 
