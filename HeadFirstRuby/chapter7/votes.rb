8bf5694220a3ace0a5a4270ef50580644bde1716
kevin <kevinochieng548@gmail.com>
2016-06-28 13:11:42 +0300

    count votes

--- HeadFirstRuby/chapter7/votes.rb
+++ HeadFirstRuby/chapter7/votes.rb
@@ -4,3 +4,23 @@ lines = []
 File.open("votes.txt") do |file|
   lines = file.readlines #store all the file lines in an array
 end
+
+p lines
+
+#set an empty hash
+
+votes = Hash.new(0) #create a new hash with a default object of zero
+
+lines.each do |line|
+  name = line.chomp
+  name.upcase! # change the name to ALL CAPS before using it as a hash key
+    votes[name] += 1 #increment whatever value is returned
+    #0 if key has never been updated or current tally otherwise
+end
+
+#process each key/value pair
+votes.each do |name,count|
+  puts "#{name}:#{count}"
+end
+
+
