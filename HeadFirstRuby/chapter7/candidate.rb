f7580792f5f51043aff084fbe0615c23e501c625
kevin <kevinochieng548@gmail.com>
2016-06-28 20:47:32 +0300

    make all the hash optional

--- HeadFirstRuby/chapter7/candidate.rb
+++ HeadFirstRuby/chapter7/candidate.rb
@@ -1,7 +1,7 @@
 class Candidate
   attr_accessor :name,:age, :occupation, :hobby, :birthplace
 
-  def initialize(name,options)
+  def initialize(name,options = {})
     self.name = name
     self.age = options[:age]
     self.occupation = options[:occupation]
@@ -13,5 +13,8 @@ end
 
 candidate = Candidate.new("Wesley Snipes",
                           age: 50, occupation:"Programmer",hobby:"Coding",birthplace:"Daadab")
+cad = Candidate.new("Amy Ngunt",age: 32, occupation: "Engineer")
 p candidate
+p Candidate.new("carl")
+p cad
 
