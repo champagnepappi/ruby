ef126a10ffcc9c69a299b0732e88229be3001ccf
kevin <kevinochieng548@gmail.com>
2016-06-28 13:12:37 +0300

    use hash parameters in Candidates class

---
+++ HeadFirstRuby/chapter7/candidate.rb
@@ -0,0 +1,17 @@
+class Candidate
+  attr_accessor :name,:age, :occupation, :hobby, :birthplace
+
+  def initialize(name,options)
+    self.name = name
+    self.age = options[:age]
+    self.occupation = options[:occupation]
+    self.hobby = options[:hobby]
+    self.birthplace = options[:birthplace]
+
+  end
+end
+
+candidate = Candidate.new("Wesley Snipes",
+                          {:age =>50, :occupation =>"Programmer",:hobby =>"Coding",:birthplace =>"Daadab"})
+p candidate
+
