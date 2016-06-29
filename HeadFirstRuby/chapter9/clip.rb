9ca4eb7ccd53a07f7b0162cfbfb438e924238e8a
kevin <kevinochieng548@gmail.com>
2016-06-29 22:38:19 +0300

    use conditional assignment operator in module

---
+++ HeadFirstRuby/chapter9/clip.rb
@@ -0,0 +1,47 @@
+module AcceptsComments
+  def comments
+    @comments ||= []
+     end
+
+  def add_comment(comment)
+    comments << comment
+  end
+end
+
+
+class Clip
+  #show id of object we are playing
+  def play
+    puts "Playing #{object_id}..." 
+  end
+
+end
+
+class Video < Clip
+  include AcceptsComments
+  attr_accessor :resolution
+end
+
+class Song < Clip
+  include AcceptsComments
+  attr_accessor :beats_per_minute  
+end
+
+class Photo
+  include AcceptsComments
+  def show
+    puts "Displaying #{object-id}"
+  end
+end
+
+video = Video.new
+video.add_comment("Some cool slow motion effect!")
+video.add_comment("Weird ending")
+
+song = Song.new
+song.add_comment("Awesome beat")
+
+photo = Photo.new
+photo.add_comment("Beautiful colors")
+
+p video.comments, song.comments, photo.comments
