424fcce9eacc283d33d3242e8e13003180bf643e
kevin <kevinochieng548@gmail.com>
2016-06-29 11:20:53 +0300

    let block return value

--- HeadFirstRuby/astronout.rb
+++ HeadFirstRuby/astronout.rb
@@ -6,15 +6,9 @@ bodies = Hash.new do |hash,key|
   body = CelestialBody.new #set up object which will become value for key 
   body.type = "planet"
   hash[key] = body #assign the object to the current hash key
-  body #return object
-end
+  
+  end
  #inspect the default object
 
-bodies['Mars'].name = 'Mars' #add value to the hash
-bodies['Europa'].name = 'Europa'
-bodies['Europa'].type = 'moon'
-bodies['Venus'].name = 'Venus'
-
 p bodies['Mars']
-p bodies['Europa']
-p bodies['Venus']
+
