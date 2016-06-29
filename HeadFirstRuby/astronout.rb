5e9a9e45acb3c248f0ddbb4aa53894c885927ab1
kevin <kevinochieng548@gmail.com>
2016-06-29 11:02:01 +0300

    create a hash default block

--- HeadFirstRuby/astronout.rb
+++ HeadFirstRuby/astronout.rb
@@ -2,12 +2,19 @@ class CelestialBody
   attr_accessor :type , :name
 end
 
-default_body = CelestialBody.new
-default_body.type = 'planet'
-bodies = Hash.new(default_body)
-
-p bodies.default #inspect the default object
+bodies = Hash.new do |hash,key|
+  body = CelestialBody.new #set up object which will become value for key 
+  body.type = "planet"
+  hash[key] = body #assign the object to the current hash key
+  body #return object
+end
+ #inspect the default object
 
 bodies['Mars'].name = 'Mars' #add value to the hash
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'moon'
+bodies['Venus'].name = 'Venus'
 
-p bodies.default #inspect the default object again
+p bodies['Mars']
+p bodies['Europa']
+p bodies['Venus']
