74805a6cf28d1262a8c4acab2a51f3f081a8f0de
kevin <kevinochieng548@gmail.com>
2016-06-30 11:08:04 +0300

    mix in Comparable

--- HeadFirstRuby/chapter10/steak.rb
+++ HeadFirstRuby/chapter10/steak.rb
@@ -1,6 +1,8 @@
 class Steak
+  include Comparable
   GRADE_SCORES= {"Prime"=> 3, "Choice"=> 2, "Select"=> 1}
   attr_accessor :grade
+
 
   def <=>(other)
     if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
