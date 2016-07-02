71bd7f09772fb285f293e0b4f7b3f4bb2d5e2c59
kevin <kevinochieng548@gmail.com>
2016-07-02 15:34:42 +0300

    use assert_equal to assert that two values are equal

--- HeadFirstRuby/chapter13/ruby_project/test/test_list_with_commas.rb
+++ HeadFirstRuby/chapter13/ruby_project/test/test_list_with_commas.rb
@@ -2,15 +2,21 @@ require 'minitest/autorun'
 require 'list_with_commas'
 
 class TestListWithCommas < Minitest::Test
+  def test_it_prints_one_word_alone
+    list = ListWithCommas.new
+    list.items = ['apple']
+    assert_equal('apple',list.join)
+  end
   def test_it_joins_two_words_with_and
     list = ListWithCommas.new
     list.items = ['apple','orange']
-    assert('apple and orange' == list.join)
+    assert_equal('apple and orange', list.join)
     
   end
   def test_it_joins_three_words_with_commas
     list = ListWithCommas.new
     list.items = ['apple','orange','pear']
+    assert_equal('apple,orange, and pear',list.join)
     
   end
 end
