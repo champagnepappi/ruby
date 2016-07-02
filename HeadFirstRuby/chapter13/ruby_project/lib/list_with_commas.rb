a0e0314a77b216f899954c986a56522877720c5b
kevin <kevinochieng548@gmail.com>
2016-07-02 15:39:39 +0300

    fix bug by adding if statement

--- HeadFirstRuby/chapter13/ruby_project/lib/list_with_commas.rb
+++ HeadFirstRuby/chapter13/ruby_project/lib/list_with_commas.rb
@@ -2,7 +2,9 @@ class ListWithCommas
   attr_accessor :items
 
   def join
-    if items.length == 2
+    if items.length == 1
+      return items[0]
+   elsif items.length == 2
       return "#{items[0]} and #{items[1]}"
     end
     last_item = "and #{items.last}"
