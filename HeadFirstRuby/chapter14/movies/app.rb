9fece5cb7233070e87d8560cd980ce9b1399b006
kevin <kevinochieng548@gmail.com>
2016-07-03 12:49:15 +0300

    setup an array of movies

--- HeadFirstRuby/chapter14/movies/app.rb
+++ HeadFirstRuby/chapter14/movies/app.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 require 'movie' #load file with movie class
 
 get('/movies') do
+ @movies = []
+ @movies[0] = Movie.new #setup new movie object
+ @movies[0].title = "Jaws"
+ @movies[1] = Movie.new
+ @movies[1].title = "Alien"
+ @movies[2] = Movie.new
+ @movies[2].title = "Terminator 2"
+
  erb :index #load "views/index.erb"
- @movie = Movie.new #setup new movie object
- @movie.title = "Jaws"
 end
