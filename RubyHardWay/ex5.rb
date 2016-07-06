848183c99e4beff6c2a6575b8e37e5ddf1be7102
kevin <kevinochieng548@gmail.com>
2016-07-06 08:51:36 +0300

    print assigned variables

---
+++ RubyHardWay/ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Santos'
+my_age = 35
+my_height =74
+my_weight = 78
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} inches tall"
+puts "He's #{my_weight} pounds heavy"
+puts "Actually that's not too heavy"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usually #{my_teeth} depending on the coffee"
+
+puts "if I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_weight + my_weight}"
+
