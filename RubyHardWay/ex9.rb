8b0cb842cf35bd99885f81c891bb906f6d3683c4
kevin <kevinochieng548@gmail.com>
2016-07-08 12:39:23 +0300

    exercise 9

---
+++ RubyHardWay/ex9.rb
@@ -0,0 +1,10 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months ="Jan\nFeb\nMar\nApr\nMay\nJune\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+we'll type anything here
+"""
+
