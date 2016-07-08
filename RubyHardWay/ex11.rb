628433e40bd6ad585bcd3d44ca78b48d74bd8d43
kevin <kevinochieng548@gmail.com>
2016-07-08 13:02:30 +0300

    get input from user

---
+++ RubyHardWay/ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you?"
+age = gets.chomp
+print "How tall?"
+height = gets.chomp
+print "Weight:"
+weight = gets.chomp
+
+puts "You are #{age} years old, #{height} tall and #{weight} kg heavy"
