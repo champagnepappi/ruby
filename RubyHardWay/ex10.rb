3b3a2a594c654fdcb6836583bb673b9ce039af55
kevin <kevinochieng548@gmail.com>
2016-07-08 12:54:52 +0300

    escape double quotes and single quote

---
+++ RubyHardWay/ex10.rb
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backlash_cat = "I'm \\ a \\cat"
+
+fat_cat = """
+A list
+\t* Cat food
+\t* Fish
+\t* catnip\n\t* Grass
+"""
+puts tabby_cat
+puts persian_cat
+puts backlash_cat
+puts fat_cat
