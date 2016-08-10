90452a89dfe9f8d45a57ddf982a5564cbd7e1614
kevin <kevinochieng548@gmail.com>
2016-08-10 13:02:38 +0300

    define functions and variables

---
+++ RubyHardWay/ex19.rb
@@ -0,0 +1,32 @@
+#def method cheese_and_crackers and pass it two arguments
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+  puts "You have #{cheese_count} cheeses!"
+  puts "You have #{boxes_of_crackers} boxes of crackers!"
+  puts "Man that's enough for a party!"
+  puts "Get a blanket.\n"
+end
+
+def area(width,length)
+  puts "area of #{width} and #{length} is: #{width*length}"
+end
+
+puts "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+
+puts "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+puts "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+
+puts "And we can combine the two, variables and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+area(4,5)
+
