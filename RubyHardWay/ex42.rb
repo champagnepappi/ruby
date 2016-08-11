d4054f5386a09ae14d8b76ad1ce2c6cd1fec30b0
kevin <kevinochieng548@gmail.com>
2016-08-11 12:37:55 +0300

    inherit implicitly

--- RubyHardWay/ex42.rb
+++ RubyHardWay/ex42.rb
@@ -2,23 +2,25 @@
 class Animal
 end
 
-## ??
+#dog is-a animal
 class Dog < Animal
   #
   def initialize(name)
+    #dog has-a name
     @name = name
   end
 end
 
-## ??
+## cat is-a animal
 class Cat < Animal
 
   def initialize(name)
+    #cat has-a name
     @name = name
   end
 end
 
-## ??
+## define class Person
 class Person
 
   def initialize(name)
@@ -31,7 +33,7 @@ class Person
   attr_accessor :pet
 end
 
-## ??
+## Employee is-a Person 
 class Employee < Person
 
   def initialize(name, salary)
@@ -41,15 +43,12 @@ class Employee < Person
 
 end
 
-## ??
 class Fish
 end
 
-## ??
 class Salmon < Fish
 end
 
-#                                                                  ## ??
 class Halibut < Fish
 end
 
@@ -57,26 +56,19 @@ end
 ## rover is-a Dog
 rover = Dog.new("Rover")
 
-## ??
+#create instance of a cat
 satan = Cat.new("Satan")
 
-## ??
 mary = Person.new("Mary")
 
-## ??
 mary.pet = satan
-
-## ??
+#frank is-a Employee
 frank = Employee.new("Frank", 120000)
 
-## ??
 frank.pet = rover
 
-## ??
 flipper = Fish.new()
 
-## ??
 crouse = Salmon.new()
 
-## ??
 harry = Halibut.new()
