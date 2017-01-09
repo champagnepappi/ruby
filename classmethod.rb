ff6bd3a5254df474f00b8cd49c207add889f6a23
kevin <kevinochieng548@gmail.com>
2017-01-09 17:02:57 +1300

    define instance method

--- classmethod.rb
+++ classmethod.rb
@@ -1,5 +1,10 @@
 class Square
+  #define method as being specific to the class
  def self.test_method 
    puts "Hello from the square class"
+ end
+
+ def test_method
+   puts "Hello from an instance of a class Square"
  end
 end
