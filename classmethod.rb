3153f83ec32933a575746482b7821540f2bbb54d
kevin <kevinochieng548@gmail.com>
2017-01-09 17:05:12 +1300

    showing the difference with output

--- classmethod.rb
+++ classmethod.rb
@@ -1,10 +1,13 @@
 class Square
   #define method as being specific to the class
  def self.test_method 
-   puts "Hello from the square class"
+   puts "Hello from the Square class"
  end
 
  def test_method
    puts "Hello from an instance of a class Square"
  end
 end
+
+Square.test_method
+Square.new.test_method
