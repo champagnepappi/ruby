657fb33d5384fded2473e96499e327ea6d5a6052
kevin <kevinochieng548@gmail.com>
2017-01-09 17:15:25 +1300

    define child class

--- inheritance.rb
+++ inheritance.rb
@@ -1,5 +1,13 @@
 class ParentClass
  def method1
-   puts "Hello from method one in parent class"
+   puts "Hello from method1 in parent class"
  end 
+
+ def method2
+   puts "Hello from method2 in parent class"
+ end
+end
+
+class ChildClass < ParentClass
+
 end
