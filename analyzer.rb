1f626bd4556a589f3e9b846524c6d02734d7e529
kevin <kevinochieng548@gmail.com>
2017-10-31 15:20:58 +0300

    remove some weird bug

--- analyzer.rb
+++ analyzer.rb
@@ -18,26 +18,28 @@ end
 puts "What do you want to do? 1) Multiply 2) Divide 3)Subtract 4)Remainder"
 prompt = gets.chomp.to_i
 
-puts "Enter in first number"
-first_num = gets.chomp.to_i
-puts "Enter your second number"
-second_num = gets.chomp.to_i
+if prompt == 1 || prompt == 2 || prompt == 3 || prompt == 4
+  puts "Enter in first number"
+  first_num = gets.chomp.to_i
+  puts "Enter your second number"
+  second_num = gets.chomp.to_i
 
 
-if prompt == 1
-  puts "You have chosen to multiply #{first_num} with #{second_num}"
-  result = multiply(first_num, second_num)
-elsif prompt == 2
-  puts "You have chosen to Divide #{first_num} with #{second_num}"
-  result = divide(first_num, second_num)
-elsif prompt == 3
-  puts "You have chosen to Subtract #{first_num} from #{second_num}"
-  result = subtract(first_num, second_num)
-elsif prompt == 4
-  puts "You have chosen to find the Remainder of #{first_num} and #{second_num}"
-  result = mod(first_num, second_num)
+  if prompt == 1
+    puts "You have chosen to multiply #{first_num} with #{second_num}"
+    result = multiply(first_num, second_num)
+  elsif prompt == 2
+    puts "You have chosen to Divide #{first_num} with #{second_num}"
+    result = divide(first_num, second_num)
+  elsif prompt == 3
+    puts "You have chosen to Subtract #{first_num} from #{second_num}"
+    result = subtract(first_num, second_num)
+  elsif prompt == 4
+    puts "You have chosen to find the Remainder of #{first_num} and #{second_num}"
+    result = mod(first_num, second_num)
+  end
+
+  puts "The result is #{result}"
 else
-  puts "You've an invalid choice"
+  puts "You've entered an invalid choice"
 end
-
-puts "The result is #{result}"
