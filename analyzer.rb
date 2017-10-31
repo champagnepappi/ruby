72472fe9b52a8de91edb8d751a4978cf93666e84
kevin <kevinochieng548@gmail.com>
2017-10-31 15:12:04 +0300

    check prompt using if else and output result

--- analyzer.rb
+++ analyzer.rb
@@ -14,23 +14,30 @@ def mod(first_num, second_num)
   first_num % second_num
 end
 
+
 puts "What do you want to do? 1) Multiply 2) Divide 3)Subtract 4)Remainder"
 prompt = gets.chomp.to_i
 
+puts "Enter in first number"
+first_num = gets.chomp
+puts "Enter your second number"
+second_num = gets.chomp
+
+
 if prompt == 1
-  puts "You have chosen to multiply"
+  puts "You have chosen to multiply #{first_num} with #{second_num}"
+  result = multiply(first_num, second_num)
 elsif prompt == 2
-  puts "You have chosen to Divide"
+  puts "You have chosen to Divide #{first_num} with #{second_num}"
+  result = divide(first_num, second_num)
 elsif prompt == 3
-  puts "You have chosen to Subtract"
+  puts "You have chosen to Subtract #{first_num} from #{second_num}"
+  result = subtract(first_num, second_num)
 elsif prompt == 4
-  puts "You have chosen to fine the Remainder"
+  puts "You have chosen to find the Remainder of #{first_num} and #{second_num}"
+  result = mod(first_num, second_num)
 else
   puts "You've an invalid choice"
 end
 
-# puts "Please enter your first number"
-# first_num = gets.chomp
-# puts "Please enter your second number"
-# second_num = gets.chomp
-
+puts "The result is #{result}"
