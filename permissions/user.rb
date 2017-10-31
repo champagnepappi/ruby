a456d1e5d0cf42a299ac08e16627b786e170a460
kevin <kevinochieng548@gmail.com>
2017-10-31 13:49:46 +0300

    modify the hash

--- permissions/user.rb
+++ permissions/user.rb
@@ -9,15 +9,16 @@ class User
     @permissions = User.permissions_from_template
   end
 
-  def save
-    self_json = {email: self.email, name: self.name, permissions: self.permissions}.to_json
-    open('users.json','a') do |file|
-      file.puts self_json
-    end
-  end
 
   def self.permissions_from_template
     file = File.read 'user_permissions_template.json'
     JSON.load(file, nil, symbolize_names: true)
+  end
+
+  def save
+    self_json = {email: @email, name: @name, permissions: @permissions}.to_json
+    open('users.json','a') do |file|
+      file.puts self_json
+    end
   end
 end
