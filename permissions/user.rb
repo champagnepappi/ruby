5db7590df2467264221227470558ea5cfb8377b0
kevin <kevinochieng548@gmail.com>
2017-10-31 13:33:40 +0300

    define initialize method

--- permissions/user.rb
+++ permissions/user.rb
@@ -2,4 +2,10 @@ require 'json'
 
 class User
   attr_accessor :email, :name, :permissions
+
+  def initialize(*args)
+    @email = args[0]
+    @name = args[1]
+    @permissions = User.permissions_from_template
+  end
 end
