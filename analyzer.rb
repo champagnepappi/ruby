81b8ba182fe33221315cde4865242a441a481c2a
kevin <kevinochieng548@gmail.com>
2017-10-31 14:11:20 +0300

    define the multiply,divide,subtract and mod methods

---
+++ analyzer.rb
@@ -0,0 +1,16 @@
+def multiply(first_num, second_num)
+  first_num * second_num
+end
+
+def divide(first_num, second_num)
+  first_num / second_num
+end
+
+def subtract(first_num, second_num)
+  second_num - first_num
+end
+
+def mod(first_num, second_num)
+  first_num % second_num
+end
+
