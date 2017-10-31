682ccf030b14d2328890305d78a9bd494d1e9e01
kevin <kevinochieng548@gmail.com>
2017-10-31 13:37:21 +0300

    define the save method

--- permissions/user.rb
+++ permissions/user.rb
@@ -8,4 +8,11 @@ class User
     @name = args[1]
     @permissions = User.permissions_from_template
   end
+
+  def save
+    self_json = {email: self.email, name: self.name, permissions: self.permissions}.to_json
+    open('users.json','a') do |file|
+      file.puts self_json
+    end
+  end
 end
