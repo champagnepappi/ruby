f16ee8ff9eb846e89238954d72ca8c78fc6d8aa9
kevin <kevinochieng548@gmail.com>
2017-10-31 14:53:33 +0300

    convert prompt to integer

--- analyzer.rb
+++ analyzer.rb
@@ -15,10 +15,22 @@ def mod(first_num, second_num)
 end
 
 puts "What do you want to do? 1) Multiply 2) Divide 3)Subtract 4)Remainder"
-prompt = gets.chomp
+prompt = gets.chomp.to_i
 
-puts "Please enter your first number"
-first_num = gets.chomp
-puts "Please enter your second number"
-second_num = gets.chomp
+if prompt == 1
+  puts "You have chosen to multiply"
+elsif prompt == 2
+  puts "You have chosen to Divide"
+elsif prompt == 3
+  puts "You have chosen to Subtract"
+elsif prompt == 4
+  puts "You have chosen to fine the Remainder"
+else
+  puts "You've an invalid choice"
+end
+
+# puts "Please enter your first number"
+# first_num = gets.chomp
+# puts "Please enter your second number"
+# second_num = gets.chomp
 
