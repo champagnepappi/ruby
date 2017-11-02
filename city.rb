4060b792211a49448be36a7c91983a0140436dd3
kevin <kevinochieng548@gmail.com>
2017-11-02 14:44:40 +0300

    define loop block

--- city.rb
+++ city.rb
@@ -2,3 +2,14 @@ dial_book = {
   "newyork" => "212",
   "newbunswick"=> "732"
 }
+
+#method to display city names
+##method to get area code, dialbook and city name
+
+loop do
+  puts "Do you want to lookup a city name?(Y/N)"
+  answer = gets.chomp
+  if answer != "Y"
+    break
+  end
+end
