7f06701553741e4f8da72c09a5889490dbaf7cfd
kevin <kevinochieng548@gmail.com>
2017-11-02 15:24:30 +0300

    define get city name method

--- city.rb
+++ city.rb
@@ -4,15 +4,20 @@ dial_book = {
   "eightown" => "412",
   "kibra"=> "633",
   "ktown" => "435"
-}
+} 
 
-#method to display city names
-##method to get area code, dialbook and city name
-
+#method to display city names 
+def get_city_names(somehash)
+  somehash.each { |k,v| puts  k }
+end
+ #method to get area code, dialbook and city name 
 loop do
   puts "Do you want to lookup a city name?(Y/N)"
   answer = gets.chomp
   if answer != "Y"
     break
   end
+
+  puts "which city do you want the area code for?"
+  prompt = gets.chomp
 end
