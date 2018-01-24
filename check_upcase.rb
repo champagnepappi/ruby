378cd9b489aada72eb38f02b0060773eca598d01
kevin <kevinochieng548@gmail.com>
2018-01-24 11:46:49 +0300

    modify the check upcase method

--- check_upcase.rb
+++ check_upcase.rb
@@ -1,11 +1,11 @@
 def check_upcase(string)
-  if string.upcase
+  if string == string.upcase
     puts "The string is in upcase"
-  elsif string.uppercase
-    puts "String is in uppercase"
+  elsif string == string.capitalize
+    puts "String is capitalized"
   else
     puts "String is not in neither upcase or uppercase"
   end
 end
 
-check_upcase("SAntos")
+check_upcase("tos")
