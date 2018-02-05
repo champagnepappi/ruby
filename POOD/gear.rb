47bfc613102017c4e563f10cf479fb2ee48c1ebe
kevin <kevinochieng548@gmail.com>
2018-02-05 22:02:28 +0300

    set rim and tire in the initialize method

--- POOD/gear.rb
+++ POOD/gear.rb
@@ -1,9 +1,11 @@
 class Gear
   attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
