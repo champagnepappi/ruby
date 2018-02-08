f56d78eaafbf6076fb3030bdec80843b8192d27f
kevin <kevinochieng548@gmail.com>
2018-02-08 21:12:30 +0300

    use respond_to? method

--- gen.rb
+++ gen.rb
@@ -3,3 +3,9 @@ puts "The id of obj is #{obj.object_id}"
 
 str = "Strings are objects too"
 puts "The id of the string is #{str.object_id}"
+
+if obj.respond_to?("talk")
+  obj.talk
+else
+  puts "Sorry, the object doesn't understand the 'talk' message"
+end
