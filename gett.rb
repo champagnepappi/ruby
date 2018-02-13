11e4216183209c227edc547db6b227fc36411e18
kevin <kevinochieng548@gmail.com>
2018-02-13 10:42:28 +0300

    loop through string

---
+++ gett.rb
@@ -0,0 +1,18 @@
+name = "Getty"
+s = name.split("")
+
+for n in s
+  puts n
+end
+
+n = 0
+while n < name.length do
+  puts name[n]
+  n += 1
+end
+
+# for n in name do
+#   print n
+# end
+#
+# name.each {|x| puts x}
