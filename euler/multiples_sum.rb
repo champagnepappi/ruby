607f1fc42ec5d43a8260c1cbc5269978710866aa
kevin <kevinochieng548@gmail.com>
2018-02-21 11:32:09 +0300

    test multiples upto 50

--- euler/multiples_sum.rb
+++ euler/multiples_sum.rb
@@ -1,8 +1,15 @@
 require './multiples.rb'
 RSpec.describe Multiples do
-  it "It returns 23 for multiples below 10" do
+  it "It returns 23 for multiples upto 10" do
     multiples = Multiples.new
     sum = multiples.sum_of_multiples(10)
     expect(sum).to eq 23
+  end
+
+  it "returns 50 for multiples upto 50" do
+    multiples = Multiples.new
+    sum = multiples.sum_of_multiples(15)
+    expect(sum).to eq 50
+    
   end
 end
