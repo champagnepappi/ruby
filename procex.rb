bcbac7396258a445ab69c3099d692456cf662791
kevin <kevinochieng548@gmail.com>
2018-03-17 11:38:05 +0300

    call proc inside a method

--- procex.rb
+++ procex.rb
@@ -4,5 +4,14 @@ t.call
 my_lambda = -> {return 1}
 puts "Lambda result: #{my_lambda.call}"
 
-my_proc = Proc.new {return 1}
-puts "Proc result: #{my_proc.call}"
+# my_proc = Proc.new {return 1}
+# puts "Proc result: #{my_proc.call}"
+
+def call_proc
+  puts "Before proc"
+  my_proc = Proc.new { return 2 }
+  my_proc.call
+  puts "After proc"
+end
+
+p call_proc
