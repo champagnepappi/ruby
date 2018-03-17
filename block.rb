de34cad1f674e7135311cbd72a7ab7c0f8404058
kevin <kevinochieng548@gmail.com>
2018-03-17 10:50:45 +0300

    define and yield block

---
+++ block.rb
@@ -0,0 +1,5 @@
+def print_once
+  yield
+end
+
+print_once { puts "Block is being run" }
--- procex.rb
+++ procex.rb
@@ -1,2 +1,8 @@
 t = Proc.new { |x,y| puts "I don't care about arguments" }
 t.call
+
+my_lambda = -> {return 1}
+puts "Lambda result: #{my_lambda.call}"
+
+my_proc = Proc.new {return 1}
+puts "Proc result: #{my_proc.call}"
