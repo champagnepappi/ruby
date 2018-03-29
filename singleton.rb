a654448723abd917b8dc2921e65e05a34f3bc4da
Kevin Santos <kevinochieng548@gmail.com>
2018-03-29 23:29:25 +0300

    create instances of Ticket class

--- singleton.rb
+++ singleton.rb
@@ -10,8 +10,16 @@ f.talk
 
 class Ticket
   attr_accessor :price
+  def Ticket.most_expensive(*tickets)
+    tickets.max_by(&:price)
+  end
 end
-def Ticket.most_expensive(*tickets)
-  tickets.max_by(&price)
-end
+
+t1 = Ticket.new
+t1.price = 23
+t2 = Ticket.new
+t2.price = 52
+t3 = Ticket.new
+t3.price = 90
+print Ticket.most_expensive(t1,t2,t3)
 
