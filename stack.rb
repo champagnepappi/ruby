42a09a532df2680deffddec98581fe60fb595c4c
Kevin Santos <kevinochieng548@gmail.com>
2018-04-16 09:07:47 +0300

    remove item from stack

--- stack.rb
+++ stack.rb
@@ -6,3 +6,10 @@ end
 a = Stack.new
 a.add_to_stack("Hula")
 a.add_to_stack("okay")
+puts "Objects currently on stack:"
+puts a.stack
+taken = a.take_from_stack
+puts "Remove this object:"
+puts taken
+puts "Now on stack:"
+puts a.stack
