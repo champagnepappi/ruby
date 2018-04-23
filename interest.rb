38874d9872aeeefb50bd826a2d0b94b1f60fca1a
Kevin Santos <kevinochieng548@gmail.com>
2018-04-23 09:22:05 +0300

    define calculate interest class in Bank account class

---
+++ interest.rb
@@ -0,0 +1,12 @@
+module InterestingBearing
+  def calculate_interest
+    puts "Placeholder! We're in module interesting"
+  end
+end
+class BankAccount
+  include InterestBearing
+  def calculate_interest
+    puts "We are in bank account class"
+    puts "we are overriding method"
+  end
+end
