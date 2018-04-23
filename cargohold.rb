81fe8ea152b141788e86b35d491cc0eec4c8c30c
Kevin Santos <kevinochieng548@gmail.com>
2018-04-23 08:58:34 +0300

    demonstrate module inclusion and inheritance

--- cargohold.rb
+++ cargohold.rb
@@ -6,7 +6,7 @@ class CargoHold
  include Stacklike 
  def load_and_report(obj)
    print "Loading object"
-   puts "obj.object_id"
+   puts obj.object_id
    add_to_stack(obj)
  end
 
---
+++ classm.rb
@@ -0,0 +1,6 @@
+class Person
+  def name(n)
+    print "HI my name is #{n}"
+  end
+end
+Person.name("ss")
