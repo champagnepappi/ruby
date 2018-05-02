cb8c55bccbf807668073515785489796afb388a9
Kevin Santos <kevinochieng548@gmail.com>
2018-05-02 09:15:09 +0300

    define method missing for person class

--- pershob.rb
+++ pershob.rb
@@ -7,4 +7,26 @@ class Person
     @friends =[]
     PEOPLE << self
   end
+  def has_hobby(hobby)
+    @hobbies << hobby
+  end
+  def has_friend(friend)
+    @friends << friend
+  end
+
+  def method_missing(m,*args)
+    method = m.to_s
+    if method.start_with?("all_with_")
+      attr = method[9..-1]
+      if self.public_method_defined?(attr)
+        PEOPLE.find_all do |person|
+          person.send(attr).include?(args[0])
+        end
+      else
+        raise ArgumentError, "Can't find #{attr}"
+      end
+    else
+      super
+    end
+  end
 end
