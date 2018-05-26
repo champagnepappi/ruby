13986cbc8845fa3542d1269e5f8c77f765097976
Kevin Santos <kevinochieng548@gmail.com>
2018-05-26 16:30:00 +0300

    test if object is an instance of class Calculator

--- TDD/test_calculator.rb
+++ TDD/test_calculator.rb
@@ -2,6 +2,8 @@ require "test/unit"
 require_relative "calculator"
 class TestCalculator < Test::Unit::TestCase  
   def test_addition
+    a= Calculator.new
+    assert_instance_of(Calculator, a)
     assert_equal(Calculator.new.add(2,4), 6)
     assert_raise (RuntimeError) {Calculator.new.add("a","b")}
   end
