d6c0e8eabc9a26d3c94c1875be2f35cf4ab2601d
Kevin Santos <kevinochieng548@gmail.com>
2018-05-26 16:07:07 +0300

    test subtraction on the calculator

--- TDD/test_calculator.rb
+++ TDD/test_calculator.rb
@@ -4,4 +4,8 @@ class TestCalculator < Test::Unit::TestCase
   def test_addition
     assert_equal(Calculator.new.add(2,4), 6)
   end
+
+  def test_substraction
+    assert_equal(Calculator.new.substract(8,2),6)
+  end
 end
