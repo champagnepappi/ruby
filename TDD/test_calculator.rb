bd4c20a5b4933a50d457f5aded48f537bc4a45f1
Kevin Santos <kevinochieng548@gmail.com>
2018-05-26 16:25:47 +0300

    test raised exception if arguments ar enot numeric

--- TDD/test_calculator.rb
+++ TDD/test_calculator.rb
@@ -3,6 +3,7 @@ require_relative "calculator"
 class TestCalculator < Test::Unit::TestCase  
   def test_addition
     assert_equal(Calculator.new.add(2,4), 6)
+    assert_raise (RuntimeError) {Calculator.new.add("a","b")}
   end
 
   def test_substraction
