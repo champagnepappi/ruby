fda47af15b7da5ab0524aa3519d3268f211baaa5
Kevin Santos <kevinochieng548@gmail.com>
2018-07-06 23:40:33 +0300

    namespace module

--- modnamesapace.rb
+++ modnamesapace.rb
@@ -5,9 +5,18 @@ module Perimeter
     end
   end
 end
+module Area
+  class Array
+    def initialize
+      @size=100
+    end
+  end
+end
 
 our_array = Perimeter::Array.new
 ruby_array = Array.new
+area = Area::Array.new
 
 p our_array.class
 p ruby_array.class
+p area.class
