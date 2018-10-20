b868f30d2fea6f6f39d52a00f69828e659b77ad4
Kevin Santos <kevinochieng548@gmail.com>
2018-10-20 22:48:13 +0300

    define name getter and setter

--- exception.rb
+++ exception.rb
@@ -2,6 +2,13 @@ class Person
   def initialize(name)
     raise ArgumentError, "No name present" if name.empty?
   end
+
+  def name
+    @name
+  end
+   def name=(name)
+     @name=name
+   end
 end
 
 santos = Person.new('')
