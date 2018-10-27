b09bd5c49d8087781431b5ab972580681b06870e
Kevin Santos <kevinochieng548@gmail.com>
2018-10-27 23:39:03 +0300

    define proc to square

--- proc.rb
+++ proc.rb
@@ -2,9 +2,11 @@
 count = Proc.new { [1,2,3,4,5].each do |i| print i end; puts  }
 your_proc = lambda {puts "Lurch: You rang?"}
 my_proc = proc {puts "Morticia: 'who was at the door, Lurch?' " }
+square= Proc.new {[4,3,2,1].each do |i| print i**2 end;}
 
 puts count.class, your_proc.class, my_proc.class
 
 count.call
 your_proc.call
 my_proc.call
+square.call
