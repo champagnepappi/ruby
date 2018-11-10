feb3f71326b9e412694baef2fe6402a88b8002cf
Kevin Santos <kevinochieng548@gmail.com>
2018-11-10 23:30:14 +0300

    use the _strptime method

--- datey.rb
+++ datey.rb
@@ -1,5 +1,7 @@
+require "date"
 def srtpdate(date)
   Date.strptime(date, '%Y/%m/%d') || Date.strptime(date, '%m/%d/%Y')
 end
 
-puts strpdate("12/01/20")
+puts srtpdate("12/01/20")
+puts Date._strptime("01/01/18", '%m/%d/%Y')
