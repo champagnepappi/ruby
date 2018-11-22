aa2820ce63fc45d8a20bcacd2e35d9d23cd4d37f
Kevin Santos <kevinochieng548@gmail.com>
2018-11-22 23:25:13 +0300

    check random num

--- chompi.rb
+++ chompi.rb
@@ -4,4 +4,12 @@ puts "Your name is #{name}"
 puts "input your age:"
 age= gets.chomp
 puts "Age is: #{age}"
+puts "Enter a lucky number to continue"
+num = gets.chomp
+random_num = rand(10)
+if num == random_num
+  puts "Yeeei, welcome aboard"
+else
+  puts "Try again, num was #{random_num}"
+end
 
