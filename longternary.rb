91d69281f7830c01bc03218e17a1c3f3dc4ebe40
Kevin Santos <kevinochieng548@gmail.com>
2018-12-03 19:03:20 +0300

    call the method

--- longternary.rb
+++ longternary.rb
@@ -1,5 +1,7 @@
 def how_many_dalmatians(n)
   dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
   respond = n<=10 ? dogs[0] : n <=50 ? dogs[1] : n==101 ? dogs[3] : dogs[2]
-  respond
+  puts respond
 end
+
+how_many_dalmatians(34)
