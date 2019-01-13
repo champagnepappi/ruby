d4c2f1d575c1ce94f034c8a758b5a3daab066a97
Kevin Santos <kevinochieng548@gmail.com>
2019-01-13 10:48:59 +0300

    define case involving ranges

--- case.rb
+++ case.rb
@@ -13,6 +13,19 @@ def my_schedule(obj)
   end
 end
 
+def my_day(day)
+  case day
+  when 1..2
+    puts "day off"
+  when 3
+    puts "projects day"
+  when 4..5
+    puts "reading books"
+  else
+    puts "client work"
+  end
+end
+
 my_schedule("Monday")
 my_schedule("Tuesday")
 my_schedule("just some other day")
