cd8003de95302216859198c03391974ee41af417
Kevin Santos <kevinochieng548@gmail.com>
2019-10-06 23:28:44 +0300

    return param passed

---
+++ sinatra/sinatra-example.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'haml'
+
+get '/' do
+ @title = 'home'
+ haml :index
+end
+
+get '/about' do
+  @title = 'about'
+  haml :about
+end
+
+get '/contact' do
+  @title = 'contact'
+  haml :contact
+end
+
+get '/meetup/:name' do
+  "Hi #{params[:name].reverse}"
+end
+
+
+
